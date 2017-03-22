124552b69ea3b0111b97862d0b39746831caf2ee
Adrian Vatchinsky <avatchinsky@LGM-01.lan>
2017-03-21 21:56:07 -0400

    unity project

---
+++ Assets/Scripts/product.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class product : MonoBehaviour {
+
+	// inputs for the product
+	public string productPrice;
+	public string productText;
+
+	// wire up the assets
+	private Text productPriceUIComponent;
+	private Text productTextUIComponent;
+
+	// Use this for initialization
+	void Start () {
+		// assign values to the ui components
+		productPriceUIComponent = GameObject.FindGameObjectWithTag("Product Price").GetComponent<Text>();
+		productPriceUIComponent.text = productPrice;
+
+		productTextUIComponent = GameObject.FindGameObjectWithTag("Product Text").GetComponent<Text>();
+		productTextUIComponent.text = productText;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
