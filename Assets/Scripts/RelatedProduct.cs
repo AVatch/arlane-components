0104c0529416420ea0f29256db48ce43a0e7de0c
Adrian Vatchinsky <avatchinsky@dhcp-ccc-3120.redrover.cornell.edu>
2017-03-22 15:26:28 -0400

    made the related items a prefab + need to make a script to set their properties programaticaly

---
+++ Assets/Scripts/RelatedProduct.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelatedProduct : MonoBehaviour {
+
+	public string relatedProduct1Title;
+	public string relatedProduct1Price;
+	public string relatedProduct1Text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
