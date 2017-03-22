f5060756e96d40082fb7f73b9de57ffe3f69ab07
Adrian Vatchinsky <avatchinsky@LGM-01.lan>
2017-03-21 22:28:53 -0400

    added a scanner object that moves up

---
+++ Assets/Scripts/Scan.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scan : MonoBehaviour {
+
+	public int offsetY;
+	public float speed = 1.0F;
+
+	private float startTime;
+	private Vector3 start;
+	private Vector3 destination;
+	private float journeyLength;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+		start = gameObject.transform.position;
+		destination = new Vector3 (start.x, start.y + offsetY, start.z);
+		journeyLength = Vector3.Distance(start, destination);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp(start, destination, fracJourney);
+	}
+}
