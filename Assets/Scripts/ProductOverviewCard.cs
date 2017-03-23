50473c0036be5f99cf6d4c20e5a1df753827ff67
Adrian Vatchinsky <avatchinsky@LGM-01.lan>
2017-03-22 21:42:27 -0400

    style sync w/ Stephen

--- Assets/Scripts/ProductOverviewCard.cs
+++ Assets/Scripts/ProductOverviewCard.cs
@@ -3,32 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class ProductOverviewCard : MonoBehaviour {
 
-public class RelatedProduct : MonoBehaviour {
-
-	public string title;
 	public string price;
-	public string text;
-	public Sprite img;
+	public string description;
 
-	private Text productTitleUIComponent;
 	private Text productPriceUIComponent;
 	private Text productTextUIComponent;
-	private Image productImageUIComponent;
 
 	// Use this for initialization
 	void Start () {
-		productTitleUIComponent = GameObject.FindGameObjectWithTag("Product Title").GetComponent<Text>();
-		productTitleUIComponent.text = title;
-
 		productPriceUIComponent = GameObject.FindGameObjectWithTag("Product Price").GetComponent<Text>();
 		productPriceUIComponent.text = price;
 
 		productTextUIComponent = GameObject.FindGameObjectWithTag("Product Text").GetComponent<Text>();
-		productTextUIComponent.text = text;
-
-		productImageUIComponent = GameObject.FindGameObjectWithTag("Product Image").GetComponent<Image>();
-		productImageUIComponent.sprite = img;
+		productTextUIComponent.text = description;
 	}
 	
 	// Update is called once per frame
